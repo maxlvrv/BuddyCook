e51c3a864a78d9890e71bbd020b4113d025bbb91
hnn04164 <hnn04164@gmail.com>
2018-08-31 16:18:10 +0100

    documentation added for raspberry pi

---
+++ Raspberyy pi/Readme.txt
@@ -0,0 +1,19 @@
+This is the  Documentation for Raspberry Pi and relevant files.
+
+dsfinal.py includes codes to be run by the Raspberry Pi. It should be auto run upon boot,
+guides can be foun in the reference section
+
+**It should be noted each DS18b20 sensors have their unique identifier numbers and the guide should be followed
+to find it
+
+
+References:
+>Running scripts upon boot
+https://www.raspberrypi.org/documentation/linux/usage/rc-local.md
+
+>Getting sensor readings with Raspberry Pi
+https://cdn-learn.adafruit.com/downloads/pdf/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing.pdf
+
+>MongoDatabase manual
+https://docs.mongodb.com/manual/
+
--- Raspberyy pi/dsfinal.py
+++ Raspberyy pi/dsfinal.py
@@ -4,30 +4,30 @@ import pymongo
 from pymongo import MongoClient
 
 os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+os.system('modprobe w1-therm')          #initialises pins
 
-temp_sensor = '/sys/bus/w1/devices/28-031725077dff/w1_slave'
+temp_sensor = '/sys/bus/w1/devices/28-031725077dff/w1_slave'            #reads the file created by the sensor
 
-uri = "mongodb://cooking:wsurop18@ds223268.mlab.com:23268/wsurop_cooking"
+uri = "mongodb://cooking:wsurop18@ds223268.mlab.com:23268/wsurop_cooking"       #connects to the MongoDB
 client = MongoClient(uri)
 
 db = client.wsurop_cooking
-glo_temp = -273
+glo_temp = -273                                             #initialises a temporary value that holds the last temepratures
 
-db.temperatures.remove({"units" : "Celsius"})
+db.temperatures.remove({"units" : "Celsius"})               #clears all data in the database upon bootup of the code
 
 db.temperatures.insert_one(
-           {'temperature': glo_temp, 'units': "Celsius"}
+           {'temperature': glo_temp, 'units': "Celsius"}        #adds a dummy data to database
         )
 
-def temp_raw():
+def temp_raw():         #reads the lines from sensor file
 
     f = open(temp_sensor, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
-def read_temp():
+def read_temp():        #extracts the temperature data only from the file
 
     lines = temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -41,19 +41,17 @@ def read_temp():
         temp_string = lines[1].strip()[temp_output+2:]
         temp_c = float(temp_string) / 1000.0
         temp_c2 = int(temp_c)
-        
-        
-        #temp_f = temp_c * 9.0 / 5.0 + 32.0
+
         return temp_c2
 
 while True:
     curr_temp = read_temp()
     
     print("waiting for temp change")
-    if glo_temp != curr_temp:
+    if glo_temp != curr_temp:               #checks if there is a temperature change
         print("temp changed!")
         print(curr_temp)
-        db.temperatures.insert_one(
+        db.temperatures.insert_one(                                 #insert temperature data to database
            {'temperature': read_temp(), 'units': "Celsius"}
         )
         glo_temp = curr_temp
